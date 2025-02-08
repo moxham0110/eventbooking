a3aeba2e270a8b0c17dfecaa7c4ea32d79f07f39
PatrickM <patrick.moxham@gmail.com>
2025-02-08 20:00:16 +0000

    Initial commit

---
+++ src/test/java/com/pmoxham/eventbooking/EventbookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pmoxham.eventbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventbookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
